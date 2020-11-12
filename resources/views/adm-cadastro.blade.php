07ff4eaa6a8977286fbaaca62c8617c1dfb74e52
aridsonf <aridsonfilho@gmail.com>
2020-11-12 13:59:33 -0300

    criação do navbar adm

--- resources/views/adm-cadastro.blade.php
+++ resources/views/adm-cadastro.blade.php
@@ -1,34 +1,32 @@
 @extends('template.app')
 
 @section('conteudo')
-
-    <div id="cadastro-paciente" class="conteiner-fluid ">
+<div id="adm" class="conteiner-fluid ">
         <div class="card border-dark mb-3 ">
-            <div class="header">
-                <a class="logo">Nome do maluco</a>
-                <div class="header-right">
-                <li class="navbar">
-                    <a class="active" href="#home">Home</a>
-                      <div class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <a class="navbar-brand" href="#">gHosp</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Cadastro
         </a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#cadastro-funcionario">Cadastrar Funicionário</a>
-                                <a class="dropdown-item" href="#cadastro-paciente">Cadastrar Paciente</a>
-                            </div>
-                        </div>
-                    <a href="#about">Informações</a>
-                </li>
-                </div>
-            </div>
-            <div class="card-body text-dark bg-dark">
-                <div class="conteiner-fluid py-3 px-lg-5 border rounded bg-gainsboro">
-                    <div class="form">
-                        <button @click="enviar()" class="btn btn-primary btn-lg btn-block">Enviar</button>
-                    </div>
-                </div>
-            </div>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/cadastro-funcionario">Cadastrar Funcionário</a>
+          <a class="dropdown-item" href="/cadastro-paciente">Cadastrar Paciente</a>
         </div>
-    </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+</div>
+
 @endsection
